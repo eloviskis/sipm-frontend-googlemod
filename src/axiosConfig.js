a64ce4a59f49c887a21484f8fa8a53264df57a26
eloviskis <eloi.santaroza@gmail.com>
2024-07-26 18:02:58 -0300

    modified:   .env 	modified:   src/axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://localhost:3001', // Atualize a porta para 3001 e use https
+  baseURL: process.env.REACT_APP_API_URL || 'https://localhost:3001', // Atualize a porta para 3001 e use https
 });
 
 export default instance;
