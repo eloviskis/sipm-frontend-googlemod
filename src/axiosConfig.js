e1c17edd4963d76ced4c72864f9b76c548805a71
Eloi Carlos Santaroza <eloi.santaroza@gmail.com>
2024-07-29 22:55:11 -0300

    modified:   .env 	modified:   src/axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://localhost:3001', // Atualize a porta para 3001 e use https
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3001/api',
+  timeout: 1000,
 });
 
 export default instance;
