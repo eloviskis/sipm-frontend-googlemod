ae0848c1caec9a694ff1ee34623cbf646972270f
eloviskis <eloi.santaroza@gmail.com>
2024-08-06 17:34:19 -0300

    modified:   README.md 	modified:   package.json 	deleted:    public/favicon.ico modified:   public/index.html 	deleted:    public/logo192.png 	deleted:    public/logo512.png 	deleted:    public/manifest.json 	deleted:    public/robots.txt 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/App.test.js 	modified:   src/axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,8 +1,37 @@
 import axios from 'axios';
+import { store } from './store'; // Assumindo que o store está exportado corretamente de 'store.js'
+import { logout } from './store/authSlice';
 
 const instance = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3001/api',
   timeout: 1000,
 });
+
+// Interceptor de requisição para adicionar o token de autenticação
+instance.interceptors.request.use(
+  (config) => {
+    const token = store.getState().auth.token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// Interceptor de resposta para lidar com erros de autenticação
+instance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      store.dispatch(logout());
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
